d1928569dfb0725617f8fdca8c1bc6f0f51e6208
ria28 <riajaiswal28@gmail.com>
2020-07-09 18:00:43 +0530

    (HARD)

--- Arrays/TrappingRainwater.cpp
+++ Arrays/TrappingRainwater.cpp
@@ -4,12 +4,40 @@
 
 using namespace std;
 
-int findWater(int arr[], int n)
+int findWater(int h[], int n)
 {
-    for (int i = 0; i < n; i++)
+    int l = 0, r = n - 1, l_max = 0, r_max = 0;
+    int ans = 0;
+
+    while (l <= r)
     {
-        // ??
+        if (h[l] < h[r])
+        {
+            if (h[l] >= l_max)
+            {
+                l_max = h[l];
+            }
+            else
+            {
+                ans += l_max - h[l];
+            }
+            l++;
+        }
+        else
+        {
+            if (h[r] > r_max)
+            {
+                r_max = h[r];
+            }
+            else
+            {
+                ans += r_max - h[r];
+            }
+            r--;
+        }
     }
+
+    return ans;
 }
 int main(int args, char **argc)
 {
