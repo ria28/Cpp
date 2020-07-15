ba69422f4bdd6355fbb3c8935535d24c9c0aa193
ria28 <riajaiswal28@gmail.com>
2020-07-15 11:51:57 +0530

    Array

--- Arrays/StockBuySell.cpp
+++ Arrays/StockBuySell.cpp
@@ -19,16 +19,46 @@ void stockBuySell(int price[], int n)
 
         i++;
 
-        cout << "Buy on day: " << buy<< "\t Sell on day: " << sell << endl;
+        cout << "Buy on day: " << buy << "\t Sell on day: " << sell << endl;
     }
+}
+// https://www.geeksforgeeks.org/maximum-profit-by-buying-and-selling-a-share-at-most-twice/
+void stockBuySell2(int price[], int n,int k)
+{
+    int i = 0;
+    int sum = 0;
+    int flag = 0;
+    while (i < n - 1)
+    {
+        if (flag < k)
+        {
+            while (price[i + 1] <= price[i])
+                i++;
+
+            int buy = i;
+            flag++;
+
+            while (price[i + 1] >= price[i])
+                i++;
+            int sell = i;
+
+            i++;
+
+            cout << "Buy on day: " << buy << "\t Sell on day: " << sell << endl;
+            sum += price[sell] - price[buy];
+        }
+    }
+    cout << sum;
 }
 
 int main(int args, char **argc)
 {
-    int price[] = {100, 180, 260, 310, 40, 535, 695};
+    int price[] = { 12, 14, 17, 10, 14, 13, 12, 15 };
     int n = sizeof(price) / sizeof(price[0]);
 
-    stockBuySell(price, n);
+    // stockBuySell(price, n);
+    int k = 3;
+    stockBuySell2(price, n,k);
 
     return 0;
 }
--- Arrays/difference.cpp
+++ Arrays/difference.cpp
@@ -28,6 +28,26 @@ bool findPair(int arr[], int len, int difference)
     return false;
 }
 
+// Maximum difference between two elements such that larger element appears after the smaller number
+// https://www.geeksforgeeks.org/maximum-difference-between-two-elements/
+
+void maxDiff(int arr[], int n)
+{
+    int max_diff = arr[1]-arr[0];
+    int min_ele = arr[0];
+
+    for(int i =1; i<n; i++)
+    {
+        if(arr[i]-min_ele>max_diff)
+            max_diff=arr[i]-min_ele;
+        if(arr[i]<min_ele)
+            min_ele= arr[i];
+    }
+
+    cout<<max_diff;
+
+}
+
 int main(int args, char **argv)
 {
     // int arr[] = {1, 8, 30, 40, 100};
@@ -35,6 +55,8 @@ int main(int args, char **argv)
     int size = sizeof(arr) / sizeof(arr[0]);
     // int n = 60;
     int n= 70;
-    findPair(arr, size, n);
+    // findPair(arr, size, n);
+
+    maxDiff(arr,size);
     return 0;
 }
