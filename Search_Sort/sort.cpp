c6d9e34819cc964dde5c235c06e66da0d5849f11
ria28 <riajaiswal28@gmail.com>
2020-07-27 18:35:14 +0530

    Added

--- Search_Sort/sort.cpp
+++ Search_Sort/sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -42,7 +43,7 @@ bool checkReverse(int arr[], int n)
         else
             break;
     }
-    startD = j-1 ;
+    startD = j - 1;
     // cout<<startD;
     for (k = j; k < n; k++)
     {
@@ -51,19 +52,49 @@ bool checkReverse(int arr[], int n)
         else
             break;
     }
-    int stopD = k-1;
+    int stopD = k - 1;
     // cout<<stopD;
 
-    if (arr[startD] > arr[stopD + 1] && stopD+1<n)
+    if (arr[startD] > arr[stopD + 1] && stopD + 1 < n)
         return false;
-    
-    if(arr[stopD]<arr[startD-1])
-    return false;
+
+    if (arr[stopD] < arr[startD - 1])
+        return false;
 
     if (k == n - 1)
         return true;
+}
 
+// NEARLY SORTED ALGORITHM (K -SORTED)============================================================
+// https://www.geeksforgeeks.org/nearly-sorted-algorithm/
 
+void sortK(int arr[], int n, int k)
+{
+    // kind of window sliding approach using min heap
+
+    // put first k element (0,1,2...k) int min heap
+    priority_queue<int, vector<int>, greater<int>> min_heap(arr, arr + k + 1);
+
+    int index = 0; // to recreate array --> result i.e, array sorted
+
+    for (int i = k + 1; i < n; i++)
+    {
+        arr[index++] = min_heap.top();
+        min_heap.pop();
+        min_heap.push(arr[i]);
+    }
+
+    // left over elements
+    while (min_heap.empty() == false)
+    {
+        arr[index++] = min_heap.top();
+        min_heap.pop();
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " , ";
+    }
 }
 
 int main(int args, char **argc)
@@ -75,7 +106,15 @@ int main(int args, char **argc)
     // int arr2[] = {1, 3, 4, 10, 9, 8};
     // int arr2[]={ 1, 2, 4, 5, 3};
     // int arr2[] ={1,2,3,4,20,9,16,17};
-    int arr2[]={1,2,3,4,20,9,21,22};
+    int arr2[] = {1, 2, 3, 4, 20, 9, 21, 22};
     int n2 = sizeof(arr2) / sizeof(arr2[0]);
-    cout << boolalpha<<checkReverse(arr2, n2);
+    // cout << boolalpha << checkReverse(arr2, n2);
+
+    
+
+    //  NEARLY SORTED ALGORITHM (K -SORTED)
+    int arr3[] = {6, 5, 3, 2, 8, 10, 9};
+    int k = 3;
+    int n3 = sizeof(arr3) / sizeof(arr3[0]);
+    sortK(arr3, n, k);
 }
