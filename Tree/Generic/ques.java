48a6ef2543144f04953c9a2168d00c20a05dea46
ria28 <riajaiswal28@gmail.com>
2020-10-19 15:27:16 +0530

    ques

--- Tree/Generic/ques.java
+++ Tree/Generic/ques.java
@@ -1,6 +1,7 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
-public class ques{
+public class ques {
 
     public static class Node {
         int data;
@@ -34,69 +35,81 @@ public class ques{
                 Node node = new Node(Integer.parseInt(data.toString()));
                 if (!st.isEmpty()) {
                     st.peek().children.add(node);
-                    
+
                 } else {
-                    // Integer.parseInt(String.valueOf(str.charAt(i - 1))); for 20 it'll take 0 this not valid we need to use data string builder
+                    // Integer.parseInt(String.valueOf(str.charAt(i - 1))); for 20 it'll take 0 this
+                    // not valid we need to use data string builder
                     root = node;
                 }
                 st.push(node);
                 data = new StringBuilder();
-            } else if(str.charAt(i) == ')'){
+            } else if (str.charAt(i) == ')') {
                 st.pop();
-            }
-            else{
-                data.append( str.charAt( i ) );
+            } else {
+                data.append(str.charAt(i));
             }
         }
 
         display(root);
     }
 
-    static Node large=null;
-    static Node slarge= null;
-    public static void secondLargest(Node node){
-        if(large == null)
+    static Node large = null;
+    static Node slarge = null;
+
+    public static void secondLargest(Node node) {
+        if (large == null)
             large = node;
-        for(Node child : node.children){
+        for (Node child : node.children) {
             secondLargest(child);
         }
 
-        if(large.data < node.data){
+        if (large.data < node.data) {
             slarge = large;
             large = node;
-        }else if(slarge.data<node.data){
+        } else if (slarge.data < node.data) {
             slarge = node;
         }
     }
 
     // https://www.geeksforgeeks.org/node-maximum-sum-immediate-children-n-ary-tree/
     static int maxSum = Integer.MIN_VALUE;
-    public static void maxSumChildren(Node node){
-        int sum =0;
-        for(Node child: node.children){
-            sum+=child.data;
+
+    public static void maxSumChildren(Node node) {
+        int sum = 0;
+        for (Node child : node.children) {
+            sum += child.data;
             maxSumChildren(child);
         }
         sum += node.data;
-        if(maxSum<sum)
+        if (maxSum < sum)
             maxSum = sum;
     }
 
     // https://www.geeksforgeeks.org/given-n-ary-tree-count-number-nodes-number-children-parent/
+    // doubtfull
     static int maxCnt = Integer.MIN_VALUE;
-    static int no =0;
-    public static int countNodes(Node node){
-        int cnt =0;
-        for(Node child: node.children){
+    static int no = 0;
+
+    public static int countNodes(Node node) {
+        int cnt = 0;
+        for (Node child : node.children) {
             cnt++;
-            int ccnt=countNodes(child);  
-            
-            if(cnt < ccnt){
+            int ccnt = countNodes(child);
+
+            if (cnt < ccnt) {
                 no++;
             }
         }
         return cnt;
     }
+
+    // https://www.geeksforgeeks.org/construct-full-k-ary-tree-preorder-traversal/
+    public static void preorderTree(int[] pre, int k) {
+        
+
+    }
+
+    // all left after this----------------  *************GENERIC PART LEFT
 
     public static void display(Node node) {
         String str = node.data + "->";
@@ -112,10 +125,11 @@ public class ques{
     }
 
     public static void main(String args[]) {
-        // int[] arr = { 10, 20, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1 };
+        // int[] arr = { 10, 20, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90,
+        // -1, -1, 40, 100, -1, -1, -1 };
 
         // int[] arr = { 10, 20, -1, 30, 50, -1, 60, -1, -1, 40, -1, -1 };
-        int[] arr = { 10, 20,30,-1,40,-1,-1,50,60,-1,70,-1,80,-1,-1,-1};
+        int[] arr = { 10, 20, 30, -1, 40, -1, -1, 50, 60, -1, 70, -1, 80, -1, -1, -1 };
 
         Stack<Node> st = new Stack<>();
         Node root = null;
